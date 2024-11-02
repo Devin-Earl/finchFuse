c32cc85993bafb68936c1c52047c43b564c56d97
Devin Earl <earlr@Devins-Air.lan>
2024-11-02 12:00:04 -0400

    Trigger redeployment

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// app.js
+function showMessage() {
+    document.getElementById('message').innerText = "Hello from vanilla JavaScript!";
+}
--- terraform/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "test-bucket-dearl-nyc"
-  acl    = "private"
-  
-}
